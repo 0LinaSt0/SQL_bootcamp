7245313f7ec3de31cdcd51894e5e932801f04487
Христофорова Полина Андреевна <hristoforova@miit.ru>
2023-02-02 10:47:43 +0300

    [02] 10: comleted

--- 02/src/ex10/day02_ex10.sql
+++ 02/src/ex10/day02_ex10.sql
@@ -1,12 +1,24 @@
 /*
-STILL DONT WORK
-NEED TO DELETE DUBLICATE
-*/
+ * Another desition (find least and greates elem in two column):
+ *
+ * 		 ___________________________________________________________
+ * 		| select distinct least(p1.name, p2.name) as person_name1,	|
+ * 		| 	greatest(p2.name, p1.name) as person_name2,				|
+ * 		| 	p1.address as common_address							|
+ * 		| from person as p1											|
+ * 		| 	join person as p2 on p2.address = p1.address			|
+ * 		| where p1.name != p2.name									|
+ * 		| order by 1, 2, 3											|
+ * 		 -----------------------------------------------------------
+ */
 
 select p1.name as person_name1,
 	p2.name as person_name2,
-    p1.address as common_address
+	p1.address as common_address
 from person as p1
 	join person as p2 on p2.address = p1.address
-where p1.name != p2.name
-order by 1, 2
+where p1.id > p2.id
+order by 1, 2, 3
+
+
+
