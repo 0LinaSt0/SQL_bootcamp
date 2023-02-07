bd5748e5a6f7055734ad8c72d0612ce7b80ffa18
Mera Salena <msalena@mi-h5.msk.21-school.ru>
2023-02-07 19:25:52 +0300

    [00] passing vertion 0.1

--- 00/src/start.sql
+++ 00/src/start.sql
@@ -5,28 +5,28 @@
  *
  * For starting script to execute command below in pgAdmin4:
  * 		 ___________________________________________________________
- * 		| $ \i /Users/$USER/Desktop/SQL_bootcamp/00/src/start.sql	|
+ * 		| $ \i /Users/msalena/Desktop/SQL_bootcamp/00/src/start.sql	|
  * 		 -----------------------------------------------------------
  */
 
  
 select '~~~~EXERCISE_00~~~~' as next;
-\i /Users/$USER/Desktop/SQL_bootcamp/00/src/ex00/day00_ex00.sql;
+\i /Users/msalena/Desktop/SQL_bootcamp/00/src/ex00/day00_ex00.sql;
 select '~~~~EXERCISE_01~~~~' as next;
-\i /Users/$USER/Desktop/SQL_bootcamp/00/src/ex01/day00_ex01.sql;
+\i /Users/msalena/Desktop/SQL_bootcamp/00/src/ex01/day00_ex01.sql;
 select '~~~~EXERCISE_02~~~~' as next;
-\i /Users/$USER/Desktop/SQL_bootcamp/00/src/ex02/day00_ex02.sql;
+\i /Users/msalena/Desktop/SQL_bootcamp/00/src/ex02/day00_ex02.sql;
 select '~~~~EXERCISE_03~~~~' as next;
-\i /Users/$USER/Desktop/SQL_bootcamp/00/src/ex03/day00_ex03.sql;
+\i /Users/msalena/Desktop/SQL_bootcamp/00/src/ex03/day00_ex03.sql;
 select '~~~~EXERCISE_04~~~~' as next;
-\i /Users/$USER/Desktop/SQL_bootcamp/00/src/ex04/day00_ex04.sql;
+\i /Users/msalena/Desktop/SQL_bootcamp/00/src/ex04/day00_ex04.sql;
 select '~~~~EXERCISE_05~~~~' as next;
-\i /Users/$USER/Desktop/SQL_bootcamp/00/src/ex05/day00_ex05.sql;
+\i /Users/msalena/Desktop/SQL_bootcamp/00/src/ex05/day00_ex05.sql;
 select '~~~~EXERCISE_06~~~~' as next;
-\i /Users/$USER/Desktop/SQL_bootcamp/00/src/ex06/day00_ex06.sql;
+\i /Users/msalena/Desktop/SQL_bootcamp/00/src/ex06/day00_ex06.sql;
 select '~~~~EXERCISE_07~~~~' as next;
-\i /Users/$USER/Desktop/SQL_bootcamp/00/src/ex07/day00_ex07.sql;
+\i /Users/msalena/Desktop/SQL_bootcamp/00/src/ex07/day00_ex07.sql;
 select '~~~~EXERCISE_08~~~~' as next;
-\i /Users/$USER/Desktop/SQL_bootcamp/00/src/ex08/day00_ex08.sql;
+\i /Users/msalena/Desktop/SQL_bootcamp/00/src/ex08/day00_ex08.sql;
 select '~~~~EXERCISE_09~~~~' as next;
-\i /Users/$USER/Desktop/SQL_bootcamp/00/src/ex09/day00_ex09.sql;
+\i /Users/msalena/Desktop/SQL_bootcamp/00/src/ex09/day00_ex09.sql;
