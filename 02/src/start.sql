175ca43bbdc70d38a9a6fa13746652786a175669
Христофорова Полина Андреевна <hristoforova@miit.ru>
2023-02-08 16:18:28 +0300

    [02] passing vertion 0.0

--- 02/src/start.sql
+++ 02/src/start.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * EXECUTE EXERCISES
  *
@@ -31,16 +30,3 @@ select '~~~~EXERCISE_08~~~~' as next;
 select '~~~~EXERCISE_09~~~~' as next;
 \i /Users/msalena/Desktop/SQL_bootcamp/02/src/ex09/day02_ex09.sql
 select '~~~~EXERCISE_10~~~~' as next;
-=======
-\i /Users/msalena/Desktop/SQL_bootcamp/02/src/ex00/day02_ex00.sql
-\i /Users/msalena/Desktop/SQL_bootcamp/02/src/ex01/day02_ex01.sql
-\i /Users/msalena/Desktop/SQL_bootcamp/02/src/ex02/day02_ex02.sql
-\i /Users/msalena/Desktop/SQL_bootcamp/02/src/ex03/day02_ex03.sql
-\i /Users/msalena/Desktop/SQL_bootcamp/02/src/ex04/day02_ex04.sql
-\i /Users/msalena/Desktop/SQL_bootcamp/02/src/ex05/day02_ex05.sql
-\i /Users/msalena/Desktop/SQL_bootcamp/02/src/ex06/day02_ex06.sql
-\i /Users/msalena/Desktop/SQL_bootcamp/02/src/ex07/day02_ex07.sql
-\i /Users/msalena/Desktop/SQL_bootcamp/02/src/ex08/day02_ex08.sql
-\i /Users/msalena/Desktop/SQL_bootcamp/02/src/ex09/day02_ex09.sql
->>>>>>> bd5748e ([00] passing vertion 0.1)
-\i /Users/msalena/Desktop/SQL_bootcamp/02/src/ex10/day02_ex10.sql
