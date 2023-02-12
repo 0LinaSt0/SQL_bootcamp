d2134884e4191832042bc572b6ea2bc916a5d2b5
Mera Salena <msalena@mi-a6.msk.21-school.ru>
2023-02-12 21:48:16 +0300

    [07] completed

--- team00/src/ex00/team00_ex00.sql
+++ team00/src/ex00/team00_ex00.sql
@@ -18,45 +18,59 @@
  *
  * Work with strings:
  * https://postgrespro.ru/docs/postgrespro/9.6/functions-string
+ *
  */
-
-create table nodes (
+ 
+ -- Data table initialization --
+drop table if exists nodes;
+create table if not exists nodes (
 	node1 bpchar not null,
 	node2 bpchar not null,
 	cost integer not null
-) ;
+);
+insert into nodes values
+	('a', 'b', 10),
+	('a', 'd', 20),
+	('a', 'c', 15),
+	('b', 'a', 10),
+	('b', 'c', 35),
+	('b', 'd', 25),
+	('c', 'a', 15),
+	('c', 'b', 35),
+	('c', 'd', 30),
+	('d', 'a', 20),
+	('d', 'b', 25),
+	('d', 'c', 30);
 
-insert into nodes values ('a', 'b', 10);
-insert into nodes values ('a', 'd', 20);
-insert into nodes values ('a', 'c', 15);
-insert into nodes values ('b', 'a', 10);
-insert into nodes values ('b', 'c', 35);
-insert into nodes values ('b', 'd', 25);
-insert into nodes values ('c', 'a', 15);
-insert into nodes values ('c', 'b', 35);
-insert into nodes values ('c', 'd', 30);
-insert into nodes values ('d', 'a', 20);
-insert into nodes values ('d', 'b', 25);
-insert into nodes values ('d', 'c', 30);
-
-create recursive view v_recursive_salesman(i_loop, total_cost, node1, node2, tour) as (
+ -- Creation of views with all possible ways --
+create recursive view v_recursive_salesman(total_cost, node1, node2, tour) as (
 	select
-		1 as i_loop,
 		cost as total_cost,
 		node1 as node1,
 		node2 as node2,
-		node1 as tour
+		array[node1] as tour
 	from nodes
 	where node1 = 'a'
 	union
 	select 
-		i_loop+1 as i_loop,
 		v_recursive_salesman.total_cost + nd.cost as total_cost,
 		nd.node1 as node1,
 		nd.node2 as node2,
 		(v_recursive_salesman.tour || nd.node1) as tour
 	from nodes nd
 		join v_recursive_salesman on v_recursive_salesman.node2 = nd.node1 and nd.node2 != v_recursive_salesman.node1
-			and (position(nd.node2 in tour) = 0
-			or (char_length(tour) = 3 and nd.node2 = 'a'))
+			and (not nd.node2 = any(tour) or (array_length(tour, 1) = 3 and nd.node2 = 'a'))
 );
+
+create or replace function f_salesman() returns table(total_cost int, tour text) language sql as $$
+	select
+		total_cost,
+		format('{%s,%s,%s,%s,%s}', tour[1], tour[2], tour[3], tour[4], 'a') as tour
+	from v_recursive_salesman
+	where array_length(tour, 1) = 4
+	order by total_cost, tour
+$$;
+
+ -- ex00 --
+select * from f_salesman()
+where total_cost = (select min(total_cost) from f_salesman())
--- team00/src/ex01/team00_ex01.sql
+++ team00/src/ex01/team00_ex01.sql
@@ -1,10 +1,20 @@
-select total_cost,
+/*
+ * Another solution with JOINs:
+ *
+	select (
+		nd1.cost + nd2.cost + nd3.cost + nd4.cost
+	) as total_cost,
 	(
-		'{' || substring(tour, 1, 1) || ',' || substring(tour, 2, 1) || ',' 
-		|| substring(tour, 3, 1) || ',' || substring(tour, 4, 1) || ',' 
-		|| 'a' || '}'
+		'{' || nd1.node1 || ',' || nd1.node2 || ',' || nd3.node1
+		|| ',' || nd3.node2 || ',' || nd4.node2 || '}'
 	) as tour
-from v_recursive_salesman
-where char_length(tour) = 4
-order by 1, 2;
+	from nodes as nd1
+		join nodes as nd2 on nd2.node1 = nd1.node2 and nd2.node2 != nd1.node1
+		join nodes as nd3 on nd3.node1 = nd2.node2 and nd3.node2 != nd2.node1
+			and nd3.node2 != nd1.node1
+		join nodes as nd4 on nd4.node1 = nd3.node2 and nd4.node2 = nd1.node1
+	where nd1.node1 = 'a'
+	order by 1, 2;
+ */
 
+select * from f_salesman();
