d702e9982571525f7761bf8d67ccb5467c63d9d2
Христофорова Полина Андреевна <hristoforova@miit.ru>
2023-02-17 16:44:26 +0300

    [09] completed

--- 09/src/ex08/day09_ex08.sql
+++ 09/src/ex08/day09_ex08.sql
@@ -1,3 +1,4 @@
+/*
 create or replace sequence seq_fibonacci
 	start 0
 	increment 1;
@@ -11,3 +12,24 @@ begin
 	)
 end;
 $$ language plpgsql;
+*/
+
+
+create or replace function fnc_fibonacci (pstop integer default 10)
+	returns table(number bigint) as
+$$
+	with recursive count as (
+		select
+			0 as number1,
+			1 as number2
+	union all
+		select
+			(c.number1 + c.number2) as number1,
+			c.number1 as number2
+		where number1 < pstop
+	)
+	select number1 as number from count limit 10
+$$ language sql;
+
+
+select * from fnc_fibonacci();
