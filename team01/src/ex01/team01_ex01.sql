e1c1cc847f80e7143fe5346ed76b9cbd4a4f7bf1
Mera Salena <msalena@at-p8.msk.21-school.ru>
2023-02-19 16:59:00 +0300

    [team01] passing vertion 0.0; [check_lists] added to 07, 08, 09

--- team01/src/ex01/team01_ex01.sql
+++ team01/src/ex01/team01_ex01.sql
@@ -1,12 +1,41 @@
 /*
-Output Column	Formula (pseudocode)
+ * Vertion with UNION ALL:
+ * _________________________________________________________________________________________________
+	create or replace function fnc_find_nearest_date(currency_id integer, balance_updated timestamp)
+		returns numeric as
+	$$
+		with cte_a as (
+			(
+				select rate_to_usd
+				from currency
+				where id = currency_id and updated < balance_updated
+				order by updated desc
+				limit 1
+			)
+			union all
+			(
+				select rate_to_usd
+				from currency
+				where id = currency_id and updated >= balance_updated
+				order by updated asc
+				limit 1
+			)
+		)
+		select *
+		from cte_a
+		limit 1
+	$$ language sql;
 
-name			source: user.name if user.name is NULL then return not defined value
-lastname		source: user.lastname if user.lastname is NULL then return not defined value
-currency_name	source: currency.name
-currency_in_usd	involved sources: currency.rate_to_usd, currency.updated, balance.updated.
-				Take a look at a graphical interpretation of the formula below.
-*/
+
+	select coalesce(u.name, 'not defined') as name,
+		coalesce(u.lastname, 'not defined') as lastname,
+		c.name as currency_name,
+		(fnc_find_nearest_date(b.currency_id, b.updated) * b.money)::real as currency_in_usd
+	from balance as b
+		left join "user" as u on u.id = b.user_id
+		join (select distinct id, name from currency) as c on b.currency_id = c.id
+	order by 1 desc, 2 asc, 3 asc;
+ */
 
 
 -- INSERING STATEMENTS FROM SUBJECT
@@ -14,44 +43,62 @@ insert into currency values (100, 'EUR', 0.85, '2022-01-01 13:29');
 insert into currency values (100, 'EUR', 0.79, '2022-01-08 13:29');
 --
 
-create or replace function fnc_find_nearest_pass_rate(current_currency_id bigint, balance_update timestamp)
+
+/* 
+ * Finds the nearest currency (to current balance updated)
+ * updated in past and returns it's rate_to_usd or NULL if
+ * there are not updated in pass
+ */
+create or replace function fnc_find_nearest_past_rate(current_currency_id bigint, balance_updated timestamp)
 	returns numeric as
 $$
 begin
 	return (
 		select rate_to_usd
 		from currency
-		where id = current_currency_id and updated < balance_update
-		order by (balance_update - updated)
+		where id = current_currency_id and updated < balance_updated
+		order by (balance_updated - updated)
 		limit 1
 	);
 end;
 $$ language plpgsql;
 
 
-create or replace function fnc_find_nearest_future_rate(current_currency_id bigint, balance_update timestamp)
+/* 
+ * Finds the nearest currency (to current balance updated)
+ * updated in future and returns it's rate_to_usd
+ */
+create or replace function fnc_find_nearest_future_rate(current_currency_id bigint, balance_updated timestamp)
 	returns numeric as
 $$
 begin
 	return (
 		select rate_to_usd
 		from currency
-		where id = current_currency_id and updated >= balance_update
-		order by (updated - balance_update)
+		where id = current_currency_id and updated >= balance_updated
+		order by (updated - balance_updated)
 		limit 1
 	);
 end;
 $$ language plpgsql;
 
 
-create or replace function fnc_find_nearest_rate(current_currency_id bigint, balance_update timestamp)
+/* 
+ * Returns rate_to_usd of currency.id which sames as 
+ * the passing current_currency_id with the rules below:
+ * 	1.	Finds the nearest date in past and returns it's rate_to_usd
+ * 	2.	If there aren't updates in past (returns NULL on first step)
+ * 		then finds the nearest updateded in future and returns it's 
+ * 		rate_to_usd
+ */
+create or replace function fnc_find_nearest_rate(current_currency_id bigint, balance_updated timestamp)
 	returns numeric as
 $$
 declare
-	pass_rate numeric := fnc_find_nearest_pass_rate(current_currency_id, balance_update);
+	pass_rate numeric := fnc_find_nearest_past_rate(current_currency_id, balance_updated);
 begin
 	if (pass_rate is not null) then return pass_rate; end if;
-	return fnc_find_nearest_future_rate(current_currency_id, balance_update);
+	return fnc_find_nearest_future_rate(current_currency_id, balance_updated);
 end;
 $$ language plpgsql;
 
@@ -65,5 +112,3 @@ from balance b
 	left join "user" as u on u.id = b.user_id
 order by 1 desc, 2 asc, 3 asc;
 
-
-	
